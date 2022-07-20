dd1fe20cc73206e21228ffc1f0d528f731578074
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-07-20 21:06:39 +0900

    Update english.js

--- english.js
+++ english.js
@@ -3,8 +3,7 @@
 //Define
 
 const discord = require("discord.js");
-const client = new discord.Client({partials: ["MESSAGE", "REACTION", "CHANNEL"]});
-const disbtn = require("discord.js-buttons")(client);
+const client = new discord.Client({intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMembers, discord.GatewayIntentBits.GuildMessages, discord.GatewayIntentBits.GuildMessageReactions], partials: [discord.Partials.Message, discord.Partials.Reaction, discord.Partials.Channel]});
 
 require('dotenv').config();
 const express = require("express");
@@ -23,20 +22,20 @@ function sleep(ms) {
 }
 const prefix = "!";
 
-//Web Page Settings
+//ウェブページ設定
 
 app.engine('.ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views_ja');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Discord
 client.on('ready', () => {
-  logger.success(`Logged in as ${client.user.tag}!`);
+  logger.success(`${client.user.tag}でログイン完了！！`);
 });
-client.on('message', async message => {
+client.on('messageCreate', async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   const args = message.content
@@ -45,48 +44,48 @@ client.on('message', async message => {
     .split(/ +/g);
   const command = args.shift().toLowerCase();
   if (command === "verify") {
-    if (!config['bot_owner_id'].includes(message.author.id)) return logger.warn("The command was executed by someone other than the bot owner.");
+    if (!config['bot_owner_id'].includes(message.author.id)) return logger.warn("Botのオーナー以外がこのコマンドを実行しました。");
     if (config['use_button']) {
-      const button = new disbtn.MessageButton()
-        .setID("verify_ja")
-        .setStyle("green")
+      const button = new discord.MessageButton()
+        .setCustomId("verify_ja")
+        .setStyle("SUCCESS")
         .setLabel("Verify");
-      message.channel.send({embed: {
+      message.channel.send({embeds: [{
         title: "About Verification",
         description: "Press the button below and an hCaptcha link for authentication will be sent to the DM.\nIf you do not receive the DM, please set the setting in the image below to 'ON'.\n(The site you received by DM may display 'deceptive site', but the site is safe, so select 'Details'-> 'visit this unsafe site'.)",
         image: {
-          url: "https://media.discordapp.net/attachments/815719965221978117/862842844582772757/unknown.png"
+          url: "https://media.discordapp.net/attachments/798922824349646938/860768481245790248/unknown.png"
         },
         color: 0x00ff00
-      }, buttons: [button]});
+      }], components: [new discord.MessageActionRow().addComponents(button)]});
     } else {
-      const msg = await message.channel.send({embed: {
+      const msg = await message.channel.send({embeds: [{
         title: "About Verification",
         description: "Press the ✅ below and an hCaptcha link for authentication will be sent to the DM.\nIf you do not receive the DM, please set the setting in the image below to 'ON'.\n(The site you received by DM may display 'deceptive site', but the site is safe, so select 'Details'-> 'visit this unsafe site'.)",
         image: {
-          url: "https://media.discordapp.net/attachments/815719965221978117/862842844582772757/unknown.png"
+          url: "https://media.discordapp.net/attachments/798922824349646938/860768481245790248/unknown.png"
         },
         color: 0x00ff00
-      }});
+      }]});
       msg.react("✅");
     }
   }
 });
-client.on('clickButton', async button => {
-  await button.think(true);
-  if (button.id === "verify_ja") {
-    const linkID = pool.createLink(button.clicker.user.id);
+client.on('interactionCreate', async interaction => {
+  if (interaction.isButton() && interaction.customId === "verify_ja") {
+    await interaction.deferReply({ ephemeral: true });
+    const linkID = pool.createLink(interaction.user.id);
     const embed = new discord.MessageEmbed()
       .setTitle('Verification System')
       .setDescription(`Please visit the following link within 15 minutes to verification.\nhttps://${config['domain']}/verify/${linkID}`)
       .setColor('BLUE');
-    button.clicker.user.send(embed).then(() => {
-    button.reply.edit("I sent it to DM.")
-    logger.pending("I sent a link to DM. Waiting for Verification.")
+    interaction.user.send({embeds: [embed]}).then(() => {
+      interaction.followUp("I sent it to DM.")
+      logger.pending("I sent a link to DM. Waiting for Verification.")
     })
     .catch(async () => {
-      logger.error(`I cannot send a dm to ${button.clicker.user.tag}!`);
-      button.reply.edit(`I couldn't send a message to <@!${button.clicker.user.id}>. Instead there is a link here.\nhttps://${config['domain']}/verify/${linkID}`)
+      logger.error(`I cannot send a dm to {interaction.user.tag}!`);
+      interaction.followUp(`I couldn't send a message to <@!${interaction.user.id}>. Instead there is a link here.\nhttps://${config['domain']}/verify/${linkID}`)
       return;
     })
   }
@@ -95,7 +94,7 @@ client.on('messageReactionAdd', async (reaction,  user) => {
   if (reaction.partial) await reaction.fetch();
   if (reaction.message.author != client.user) return;
   if (reaction.emoji.name != "✅") return;
-  if (!reaction.message.embeds[0] || !reaction.message.embeds[0].description.match(/sent/)) return;
+  if (!reaction.message.embeds[0] || !reaction.message.embeds[0].description.match(/送信されます/)) return;
   if (user.bot) return;
   await reaction.users.remove(user);
   const linkID = pool.createLink(user.id);
@@ -103,15 +102,17 @@ client.on('messageReactionAdd', async (reaction,  user) => {
     .setTitle('Verification System')
     .setDescription(`Please visit the following link within 15 minutes to verification.\nhttps://${config['domain']}/verify/${linkID}`)
     .setColor('BLUE');
-  user.send(embed).then(async () => {
+  user.send({embeds: [embed]}).then(async () => {
     const msg = await reaction.message.channel.send("I sent it to DM.");
     logger.pending("I sent a link to DM. Waiting for Verification.")
-    msg.delete({ timeout: 3000 });
+    await sleep(3000)
+    msg.delete();
   })
   .catch(async () => {
     logger.error(`I cannot send a dm to ${user.tag}!`);
     const msg = reaction.message.channel.send(`I couldn't send a message to <@!${user.id}>. Instead there is a link here.\nhttps://${config['domain']}/verify/${linkID}`);
-    msg.delete({ timeout: 10000 });
+    await sleep(10000)
+    msg.delete();
   });
 });
 
@@ -154,12 +155,10 @@ if (!process.env['site_key'] || !process.env['secret_key']) {
   process.exit(1)
 }
 
-
 app.listen(80, () => logger.success("Server Started"))
 client.login(process.env['token'])
 .then(() => logger.pending("Logged in..."))
 .catch(() => logger.fatal("Login error!"));
-
 app.use(function(req, res, next){
   res.status(404);
   res.render("notfound", {});
