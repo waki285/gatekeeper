21f972fae09a8f0e7dc97350144ecf7fe05bf269
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-07-24 11:38:01 +0900

    Update english.js

--- english.js
+++ english.js
@@ -2,8 +2,10 @@
 
 //Define
 
+const builders = require("@discordjs/builders")
 const discord = require("discord.js");
-const client = new discord.Client({intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMembers, discord.GatewayIntentBits.GuildMessages, discord.GatewayIntentBits.GuildMessageReactions], partials: [discord.Partials.Message, discord.Partials.Reaction, discord.Partials.Channel]});
+const client = new discord.Client({intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMembers, discord.GatewayIntentBits.GuildMessages
+, discord.GatewayIntentBits.GuildMessageReactions, discord.GatewayIntentBits.MessageContent], partials: [discord.Partials.Message, discord.Partials.Reaction, discord.Partials.Channel]});
 
 require('dotenv').config();
 const express = require("express");
@@ -22,18 +24,18 @@ function sleep(ms) {
 }
 const prefix = "!";
 
-//ウェブページ設定
+//Web Page Settings
 
 app.engine('.ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.set('views', __dirname + '/views_ja');
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Discord
 client.on('ready', () => {
-  logger.success(`${client.user.tag}でログイン完了！！`);
+  logger.success(`Log in with ${client.user.tag}!`);
 });
 client.on('messageCreate', async message => {
   if (message.author.bot) return;
@@ -44,12 +46,12 @@ client.on('messageCreate', async message => {
     .split(/ +/g);
   const command = args.shift().toLowerCase();
   if (command === "verify") {
-    if (!config['bot_owner_id'].includes(message.author.id)) return logger.warn("Botのオーナー以外がこのコマンドを実行しました。");
+    if (!config['bot_owner_id'].includes(message.author.id)) return logger.warn("Someone other than the bot owner executed this command.");
     if (config['use_button']) {
-      const button = new discord.MessageButton()
+      const button = new builders.ButtonBuilder()
         .setCustomId("verify_ja")
-        .setStyle("SUCCESS")
-        .setLabel("Verify");
+        .setStyle("Success")
+        .setLabel("verify");
       message.channel.send({embeds: [{
         title: "About Verification",
         description: "Press the button below and an hCaptcha link for authentication will be sent to the DM.\nIf you do not receive the DM, please set the setting in the image below to 'ON'.\n(The site you received by DM may display 'deceptive site', but the site is safe, so select 'Details'-> 'visit this unsafe site'.)",
@@ -57,7 +59,7 @@ client.on('messageCreate', async message => {
           url: "https://media.discordapp.net/attachments/798922824349646938/860768481245790248/unknown.png"
         },
         color: 0x00ff00
-      }], components: [new discord.MessageActionRow().addComponents(button)]});
+      }], components: [new discord.ActionRowBuilder().addComponents(button)]});
     } else {
       const msg = await message.channel.send({embeds: [{
         title: "About Verification",
@@ -72,20 +74,20 @@ client.on('messageCreate', async message => {
   }
 });
 client.on('interactionCreate', async interaction => {
-  if (interaction.isButton() && interaction.customId === "verify_ja") {
+  if (interaction.type === discord.InteractionType.MessageComponent  && interaction.customId === "verify_ja") {
     await interaction.deferReply({ ephemeral: true });
     const linkID = pool.createLink(interaction.user.id);
-    const embed = new discord.MessageEmbed()
+    const embed = new builders.EmbedBuilder()
       .setTitle('Verification System')
       .setDescription(`Please visit the following link within 15 minutes to verification.\nhttps://${config['domain']}/verify/${linkID}`)
-      .setColor('BLUE');
+      .setColor(3447003);
     interaction.user.send({embeds: [embed]}).then(() => {
       interaction.followUp("I sent it to DM.")
       logger.pending("I sent a link to DM. Waiting for Verification.")
     })
     .catch(async () => {
-      logger.error(`I cannot send a dm to {interaction.user.tag}!`);
-      interaction.followUp(`I couldn't send a message to <@!${interaction.user.id}>. Instead there is a link here.\nhttps://${config['domain']}/verify/${linkID}`)
+      logger.error(`I cannot send a dm to ${interaction.user.tag}`);
+      interaction.followUp(`I couldn't send a message to <@!${interaction.user.tag}>. Instead there is a link here.\nhttps://${config['domain']}/verify/${linkID}`)
       return;
     })
   }
@@ -94,14 +96,14 @@ client.on('messageReactionAdd', async (reaction,  user) => {
   if (reaction.partial) await reaction.fetch();
   if (reaction.message.author != client.user) return;
   if (reaction.emoji.name != "✅") return;
-  if (!reaction.message.embeds[0] || !reaction.message.embeds[0].description.match(/送信されます/)) return;
+  if (!reaction.message.embeds[0] || !reaction.message.embeds[0].description.match(/sent/)) return;
   if (user.bot) return;
   await reaction.users.remove(user);
   const linkID = pool.createLink(user.id);
-  const embed = new discord.MessageEmbed()
+  const embed = new builders.EmbedBuilder()
     .setTitle('Verification System')
     .setDescription(`Please visit the following link within 15 minutes to verification.\nhttps://${config['domain']}/verify/${linkID}`)
-    .setColor('BLUE');
+    .setColor(3447003);
   user.send({embeds: [embed]}).then(async () => {
     const msg = await reaction.message.channel.send("I sent it to DM.");
     logger.pending("I sent a link to DM. Waiting for Verification.")
@@ -136,7 +138,7 @@ app.post("/verify/:verifyId?", async (req, res) => {
   if (!pool.isValidLink(req.params.verifyId)) return res.render("invalidLink", {});
   try {
     const guild = await client.guilds.cache.get(config['guild_id']);
-    const member = guild.member(pool.getDiscordId(req.params.verifyId));
+    const member = guild.members.cache.get(pool.getDiscordId(req.params.verifyId))
     const roles = config['role_id'];
     for (let roleid of roles) {
       member.roles.add(roleid);
@@ -144,9 +146,9 @@ app.post("/verify/:verifyId?", async (req, res) => {
     }
     logger.success(`Granted a role to ${member.user.tag}.`);
     member.send("Verification Complete!")
-    res.render("valid")
+    res.render("valid", {});
   } catch(err) {
-    logger.error(`I cannot grant a role to ${pool.getDiscordId(req.params.verifyId)}! Reason: ${err}`);
+     logger.error(`I cannot grant a role to ${pool.getDiscordId(req.params.verifyId)}! Reason: ${err}`);
   }
 });
 
@@ -155,10 +157,12 @@ if (!process.env['site_key'] || !process.env['secret_key']) {
   process.exit(1)
 }
 
+
 app.listen(80, () => logger.success("Server Started"))
 client.login(process.env['token'])
 .then(() => logger.pending("Logged in..."))
 .catch(() => logger.fatal("Login error!"));
+
 app.use(function(req, res, next){
   res.status(404);
   res.render("notfound", {});
